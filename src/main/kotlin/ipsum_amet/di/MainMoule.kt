9fb57ee7d9d5cb000e04a48a58949756b2c935d6
skeizable <kibet.koskei@proton.me>
2023-09-06 03:59:02 +0300

    finish on dependency objects

--- src/main/kotlin/ipsum_amet/di/MainMoule.kt
+++ src/main/kotlin/ipsum_amet/di/MainMoule.kt
@@ -5,6 +5,10 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import kotlinx.serialization.json.Json
 import io.ktor.serialization.kotlinx.json.*
+import ipsum_amet.data.models.repository.PostDataSource
+import ipsum_amet.data.models.repository.PostDataSourceImpl
+import ipsum_amet.service.PostService
+import ipsum_amet.service.PostServiceImp
 import kotlinx.serialization.ExperimentalSerializationApi
 import org.koin.dsl.module
 import org.litote.kmongo.coroutine.CoroutineDatabase
@@ -15,6 +19,14 @@ val mainModule = module(createdAtStart = true) {
     single { provideDatabase() }
 
     single { provideNormalHttpClient() }
+
+    single<PostDataSource> {
+        PostDataSourceImpl(get())
+    }
+
+    single<PostService> {
+        PostServiceImp(get())
+    }
 }
 
 private fun provideDatabase() : CoroutineDatabase {
@@ -23,7 +35,7 @@ private fun provideDatabase() : CoroutineDatabase {
 
     return  KMongo.createClient("mongodb+srv://osmontmagnus:0svIoHKqA0RaMnTr@cluster0.v59rl4k.mongodb.net/?retryWrites=true&w=majority")
         .coroutine
-        .getDatabase("BACKEND-ASSEMENT-PROMPT-NA'AMAL")
+        .getDatabase("B-E-S-P_DB")
 }
 
 @OptIn(ExperimentalSerializationApi::class)
